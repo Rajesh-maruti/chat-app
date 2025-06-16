869254d4253e02638b243a229002fa7d23c15cea
Rajesh <rajeshmaruti07@gmail.com>
2025-06-16 20:54:39 +0530

    Updated Package.json

--- src/App.tsx
+++ src/App.tsx
@@ -34,7 +34,7 @@ function App() {
               }
             />
             <Route
-              path=""
+              path="/"
               element={
                 <PublicRoute>
                   <Login />
